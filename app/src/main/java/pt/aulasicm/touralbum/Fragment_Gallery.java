e8576f2eb04855a9885081cfc56c8f2d0ffeaa61
diogoemoreira <diogo.e.moreira@hotmail.com>
2020-11-17 17:07:54 +0000

    Main navs started

---
+++ app/src/main/java/pt/aulasicm/touralbum/Fragment_Gallery.java
@@ -0,0 +1,22 @@
+package pt.aulasicm.touralbum;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_Gallery extends Fragment {
+    public Fragment_Gallery() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment__gallery, container, false);
+    }
+}
