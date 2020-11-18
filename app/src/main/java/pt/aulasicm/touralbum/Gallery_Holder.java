bb10e50e253276b888dc6f9977f79abf429a7298
diogoemoreira <diogo.e.moreira@hotmail.com>
2020-11-18 15:01:04 +0000

    gallery holder created

---
+++ app/src/main/java/pt/aulasicm/touralbum/Gallery_Holder.java
@@ -0,0 +1,23 @@
+package pt.aulasicm.touralbum;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Gallery_Holder extends Fragment {
+
+    public Gallery_Holder() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.gallery_holder, container, false);
+    }
+}
